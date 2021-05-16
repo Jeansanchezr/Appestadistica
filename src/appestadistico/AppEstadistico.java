8a3b710a8119ff91e71e7e0946ee96e829a2be8b
jeansanchezr <jdsanchezrios@gmail.com>
2021-05-16 08:48:54 -0500

    Version 1.0

---
+++ src/appestadistico/AppEstadistico.java
@@ -0,0 +1,16 @@
+
+package appestadistico;
+
+public class AppEstadistico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //mainApp app = new mainApp();
+        
+    }
+    
+}
